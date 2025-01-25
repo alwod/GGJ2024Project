8443d726b575a2bb872fb0275303abdc1ed46c7e
Floffah <floffah@floffah.dev>
2025-01-25 16:08:20 +0000

    fix(duck): dont teleport when captured by bubble

--- ggj2024source/Scripts/bubble.gd
+++ ggj2024source/Scripts/bubble.gd
@@ -1,4 +1,5 @@
 extends Area2D
+class_name Bubble
 
 @export var float_speed := 2.0
 var hasCaptured = false
@@ -12,10 +13,23 @@ func _on_body_entered(body):
 	if(body.is_in_group("bubbleable")):
 		hasCaptured = true;
 		bodyCaptured = body
+
+		if body.name == "Duck":
+			var duck: Duck = body
+			duck.set_freeze_enabled(true);
+			duck.set_physics_process(false)
+			duck.set_process(false)
 	
 func _physics_process(delta):
 	position.y -= float_speed
 	if (hasCaptured):
+		if bodyCaptured.name == "Duck":
+			PhysicsServer2D.body_set_state(
+ 				bodyCaptured.get_rid(),
+				PhysicsServer2D.BODY_STATE_TRANSFORM,
+				Transform2D.IDENTITY.translated(global_position)
+			)
+
 		bodyCaptured.position = global_position
 	
 func float_upwards():
@@ -23,4 +37,10 @@ func float_upwards():
 
 
 func _on_timer_timeout():
+	if bodyCaptured && bodyCaptured.name == "Duck":
+		var duck: Duck = get_node("/root/Game/Duck")
+		duck.set_freeze_enabled(false)
+		duck.set_physics_process(true)
+		duck.set_process(true)
+
 	queue_free()
