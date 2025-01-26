5cc32cd5b1facb7af161fec52b32c73980ab2746
Mikha <mikharohrs@gmail.com>
2025-01-26 15:44:33 +0000

    Made duck jump on objects

--- ggj2024source/Scripts/duck_2.gd
+++ ggj2024source/Scripts/duck_2.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 600.0
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = -350.0
 
 @export var push_force := 100
+@export var jump_time := 1000
+var last_jump_time := 0
 
 
 func _physics_process(delta: float) -> void:
@@ -13,8 +15,20 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("duck_jump") and is_on_floor():
+	#if Input.is_action_just_pressed("duck_jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		
+	var time_since_last_jump := Time.get_ticks_msec() - last_jump_time
+
+	if Input.is_action_pressed("duck_jump") and time_since_last_jump > (jump_time * 1.5):
+		last_jump_time = Time.get_ticks_msec()
+
+	if time_since_last_jump < (jump_time / 2):
 		velocity.y = JUMP_VELOCITY
+
+	if time_since_last_jump > (jump_time / 2) && time_since_last_jump < jump_time:
+		#apply_impulse(Vector2(0, jump_potential))
+		pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
