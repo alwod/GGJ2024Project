97a7f6a24378c0198c646d0cc449c0a69d6b284c
Quickmotions <fergusbhaak@gmail.com>
2025-01-26 11:24:11 +0000

    stuff

--- ggj2024source/Scripts/bubble_spawner.gd
+++ ggj2024source/Scripts/bubble_spawner.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
+@onready var timer_node = $Timer
 @export var bubble: PackedScene
+@export var spawn_delay: float = 2
 var rng = RandomNumberGenerator.new()
 
+func _ready():
+	timer_node.wait_time = spawn_delay
 
 func gen_random_pos():
 	var rect : Rect2 = $CollisionShape2D.shape.get_rect()
 	var x = randi_range(rect.position.x, rect.position.x+rect.size.x)
 	var y = randi_range(rect.position.y, rect.position.y+rect.size.y)
-	var pos = Vector2(x,y) + global_position
+	var pos = Vector2(x,y) 
 	return pos
 
 func spawn_bubble():
@@ -16,7 +20,8 @@ func spawn_bubble():
 	bubbleNode.global_position = gen_random_pos()
 	bubbleNode.set_bubble_settings(rng.randf_range(10, 16), rng.randf_range(6, 10))
 	add_child(bubbleNode)
-	
+
+
 func _on_timer_timeout():
 	print("bubble spawned")
 	spawn_bubble()
