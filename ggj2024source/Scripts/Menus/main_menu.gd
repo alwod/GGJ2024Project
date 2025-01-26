45f64e918bbb2293167f9aea601a8b84dd2c3231
Quickmotions <fergusbhaak@gmail.com>
2025-01-26 15:10:05 +0000

    volume and quit

--- ggj2024source/Scripts/Menus/main_menu.gd
+++ ggj2024source/Scripts/Menus/main_menu.gd
@@ -2,3 +2,12 @@ extends Node
 
 func _on_start_game_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/comic_strip.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value/1000)
+	print("game volume set %s" % value)
