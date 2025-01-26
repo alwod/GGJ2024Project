41782537f03f0872553ebacdb5dc271be4f18458
Mikha <mikharohrs@gmail.com>
2025-01-26 15:06:32 +0000

    Added world border and drain now leads to end cutscene

--- ggj2024source/Drain/drain.gd
+++ ggj2024source/Drain/drain.gd
@@ -14,13 +14,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if is_plug_open && should_move:
 		position.y -= rising_speed
-		if position.y == rise_to_level:
+		if position.y <= rise_to_level:
 			should_move = false
 
 func _on_plug_body_entered(body: Node2D) -> void:
 	if body.is_in_group("fish") && is_plug_open:
 		# Should replace with a change to main menu scene once it's implemented
-		get_tree().quit()
+		get_tree().change_scene_to_file("res://Scenes/outro_cutscene.tscn")
 
 
 func _on_chain_body_entered(body: Node2D) -> void:
